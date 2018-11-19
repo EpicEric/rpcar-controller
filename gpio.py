236f2fd1ea515382535517a4acb6fc638637cff2
Eric Pires <ericpires9@gmail.com>
2018-11-19 20:23:42 -0200

    Remove trailing whitespace

--- gpio.py
+++ gpio.py
@@ -14,20 +14,19 @@ def gpios():
     right_direction = LED(GPIOMapping.RIGHT_DIRECTION.value)
     left_intensity = PWMLED(GPIOMapping.LEFT_INTENSITY.value)
     right_intensity = PWMLED(GPIOMapping.RIGHT_INTENSITY.value)
-    
 
     def __send_to_gpio(gpio, direction):
         if direction.value:
             gpio.on()
         else:
             gpio.off()
-    
+
     def __send_to_pwm(pwm, direction, value):
         if direction.value:
             pwm.value = 1.0 - value
         else:
             pwm.value = value
-    
+
     def to_gpio_signal(engine_event):
         left = engine_event.left_engine_input
         right = engine_event.right_engine_input
